86ac732c05c9a3db4977944936920a3918df0e3d
Issa <issakakar001@gmail.com>
2025-08-22 02:49:50 -0700

    chore(test): exclude node_modules from vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     include: ['**/src/**/*.test.{ts,tsx}'],
+    exclude: ['**/node_modules/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'json-summary'],
